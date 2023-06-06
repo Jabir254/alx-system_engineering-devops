74753d55990633bb1a6f8a4576e053249298a62b
jabir254 <jabirkullow@gmail.com>
2023-06-06 15:43:22 +0300

    number of subcribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A function that queries the Reddit API"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    headers= {"User-Agent":"Subreddit Subscriber Counter (by /u/YourUsername)"}
+
+    try:
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            return data["data"]["subscribers"]
+        else:
+            return 0
+    except requests.exceptions.RequestException:
+
+        return 0
