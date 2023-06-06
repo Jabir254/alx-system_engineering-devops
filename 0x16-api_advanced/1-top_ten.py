748adf5797ae66ee2bc16c7afb3244c2f2c99455
jabir254 <jabirkullow@gmail.com>
2023-06-06 16:10:11 +0300

    prints the titles of the first 10 hot list on subreddit on programming

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""Prints the first 10 hot posts listed for a given subreddit"""
-
-
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    headers = {"User-Agent": "Top Ten Post Printer (by /u/YourUsername)"}
-
-    try:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-        response = requests.get(url, headers=headers)
-
-        # check if the response is successful
-        if response.status_code == 200:
-            data = response.json()
-            posts = data["data"]["children"]
-            for post in posts:
-                title = post["data"]["title"]
-                print(title)
-
-        else:
-            print(None)
-
-    except requests.exceptions.RequestException:
-        print(None)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/jbir)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
